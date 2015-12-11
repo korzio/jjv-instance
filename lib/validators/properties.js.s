357d769007d4bc54da953a78853a47cd145d3e02
Alexander Korzhikov <alexanderko@backbase.com>
2015-12-11 15:12:04 +0100

    Added draft of test suite

---
+++ lib/validators/properties.js.s
@@ -0,0 +1,87 @@
+module.exports = function allOf(env, schemaStack, objectStack, options) {
+	var objectStackLast = objectStack.length - 1,
+		object = objectStack[objectStackLast].object,
+		name = objectStack[objectStackLast].key,
+		prop = object[name];
+
+	if (Array.isArray(prop)) {
+		return;
+	}
+
+	var schema = schemaStack[schemaStack.length - 1];
+	props = [];
+	objerrs = {};
+	for (p in prop)
+		if (prop.hasOwnProperty(p))
+			props.push(p);
+
+	if (options.checkRequired && schema.required) {
+		for (i = 0, len = schema.required.length; i < len; i++)
+			if (!prop.hasOwnProperty(schema.required[i])) {
+				objerrs[schema.required[i]] = { 'required': true };
+				malformed = true;
+			}
+	}
+
+	hasProp = schema.hasOwnProperty('properties');
+	hasPattern = schema.hasOwnProperty('patternProperties');
+	if (hasProp || hasPattern) {
+		i = props.length;
+		while (i--) {
+			matched = false;
+			if (hasProp && schema.properties.hasOwnProperty(props[i])) {
+				matched = true;
+				objerr = checkValidity(env, schemaStack.concat(schema.properties[props[i]]), objectStack.concat({ object: prop, key: props[i] }), options);
+				if (objerr !== null) {
+					objerrs[props[i]] = objerr;
+					malformed = true;
+				}
+			}
+			if (hasPattern) {
+				for (p in schema.patternProperties)
+					if (schema.patternProperties.hasOwnProperty(p) && props[i].match(p)) {
+						matched = true;
+						objerr = checkValidity(env, schemaStack.concat(schema.patternProperties[p]), objectStack.concat({ object: prop, key: props[i] }), options);
+						if (objerr !== null) {
+							objerrs[props[i]] = objerr;
+							malformed = true;
+						}
+					}
+			}
+			if (matched)
+				props.splice(i, 1);
+		}
+	}
+
+	if (options.useDefault && hasProp && !malformed) {
+		for (p in schema.properties)
+			if (schema.properties.hasOwnProperty(p) && !prop.hasOwnProperty(p) && schema.properties[p].hasOwnProperty('default'))
+				prop[p] = utils.clone(schema.properties[p]['default']);
+	}
+
+	if (options.removeAdditional && hasProp && schema.additionalProperties !== true && typeof schema.additionalProperties !== 'object') {
+		for (i = 0, len = props.length; i < len; i++)
+			delete prop[props[i]];
+	} else {
+		if (schema.hasOwnProperty('additionalProperties')) {
+			if (typeof schema.additionalProperties === 'boolean') {
+				if (!schema.additionalProperties) {
+					for (i = 0, len = props.length; i < len; i++) {
+						objerrs[props[i]] = { 'additional': true };
+						malformed = true;
+					}
+				}
+			} else {
+				for (i = 0, len = props.length; i < len; i++) {
+					objerr = checkValidity(env, schemaStack.concat(schema.additionalProperties), objectStack.concat({ object: prop, key: props[i] }), options);
+					if (objerr !== null) {
+						objerrs[props[i]] = objerr;
+						malformed = true;
+					}
+				}
+			}
+		}
+	}
+	if (malformed)
+		return { 'schema': objerrs };
+};
